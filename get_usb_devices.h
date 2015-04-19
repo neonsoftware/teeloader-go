0d99735e10c40389c94181c9cb69b99e234771ac
showandtell <showandtell@showandtells-MacBook-Pro.local>
2015-04-19 16:40:29 +0200

    Getting devices list

---
+++ get_usb_devices.h
@@ -0,0 +1,48 @@
+#include "libusb-1.0/libusb.h"
+
+#define TEENSY_VENDOR_ID 0x16c0
+
+
+int getConnectedTeensys (char * out)
+{
+	int teensy_found = 0;
+
+	// discover devices
+	libusb_device **list;
+	libusb_context *context = NULL;
+
+	int result = libusb_init(&context); 
+
+	if (0 > result)
+ 	{
+   		fprintf(stderr, "libusb_init() failed with %d.\n", result);
+   		return -1;
+ 	}
+
+	ssize_t cnt = libusb_get_device_list(context, &list);
+
+	//printf("%lu devices found\n", cnt);
+
+	ssize_t i = 0;
+	
+	for (i = 0; i < cnt; i++) {
+	    libusb_device *device = list[i];
+	    struct libusb_device_descriptor desc;
+	    if ( ( libusb_get_device_descriptor(device, &desc) == 0 ) && ( desc.idVendor == TEENSY_VENDOR_ID ) )
+    	{
+    		printf("[%zd] Teensy : VendorId : 0x%.4x - ProductId : 0x%.4x\n", i, desc.idVendor, desc.idProduct);
+    		sprintf(out, "0x%.4x#", desc.idProduct);
+    		teensy_found ++;
+    		out += 7;
+    	}
+	}
+	// if (found) {
+	//     libusb_device_handle *handle;
+	//     err = libusb_open(found, &handle);
+	//     if (err)
+	//         perror("Danger !");
+	//     // etc
+	// }
+	libusb_free_device_list(list, 1);
+	return teensy_found;
+}
--- teeloader_linux.go
+++ teeloader_linux.go
@@ -4,11 +4,12 @@ package teeloader
 #cgo CFLAGS: -DUSE_LIBUSB  -Wall -O2
 #cgo LDFLAGS:  -lusb
 #include "load_linux_only.h"
+#include "get_usb_devices.h"
 */
 import "C"
 import "fmt"
-//import "unsafe"
-//import "strings"
+import "unsafe"
+import "strings"
 
 func Teensy_load(mmcu string, vendor_id string, device_id string, hex_path string, wait int, hard_reboot int, no_reboot int, verbose int) int {
 	fmt.Println("Mcu: ", mmcu)
@@ -21,4 +22,23 @@ func Teensy_load(mmcu string, vendor_id string, device_id string, hex_path strin
 	fmt.Println("Verbose: ", verbose)
 	C.load_teensy_with_options(C.CString(mmcu), C.CString(vendor_id), C.CString(device_id), C.CString(hex_path), C.int(wait), C.int(hard_reboot), C.int(no_reboot), C.int(verbose))
 	return 0
+}
+
+func GetConnectedUSBTeensy() []string {
+
+	buffer := make([]byte, 256)
+	productIds := []string {}
+
+	teensys := C.getConnectedTeensys( (*C.char)(unsafe.Pointer(&buffer[0])) )
+
+	if ( teensys > 0 ){
+		for _, productId := range strings.Split( string(buffer) ,"#") {
+			if ( productId[0] == '0' ){
+				//fmt.Println("Adding : ", productId, " which is not null")
+				productIds = append ( productIds , productId )
+			}
+		}
+	}
+
+	return productIds
 }
