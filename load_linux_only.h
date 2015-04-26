ab04fcf9ba1cd759ff1bac3cce118874f0324bcc
showandtell <showandtell@showandtells-MacBook-Pro.local>
2015-04-26 14:00:31 +0200

    using ndef instead

--- load_linux_only.h
+++ load_linux_only.h
@@ -371,7 +371,7 @@ int hard_reboot(void)
 /*                                                              */
 /****************************************************************/
 
-#if defined(USE_WIN32)
+#ifndef(NO_USE_WIN32)
 
 // http://msdn.microsoft.com/en-us/library/ms790932.aspx
 #include <windows.h>
